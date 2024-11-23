97778237002241ac82fe6d83b73bf27054b311ad
juliacazeri <juliaczanatta@gmail.com>
2024-11-23 20:56:55 -0300

    Create Professor.kt

---
+++ Professor.kt
@@ -0,0 +1,11 @@
+package com.example.academia
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Professor(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val nome: String,
+    val contato: String,
+    val especializacoes: String
+)
